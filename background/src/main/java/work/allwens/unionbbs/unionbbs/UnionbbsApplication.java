e4448c43ffb51dbdc8aca5142165e09f7a18a930
amtoaer <amtoaer@outlook.com>
2020-11-03 11:24:35 +0800

    feat: 项目初始化

---
+++ background/src/main/java/work/allwens/unionbbs/unionbbs/UnionbbsApplication.java
@@ -0,0 +1,13 @@
+package work.allwens.unionbbs.unionbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnionbbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnionbbsApplication.class, args);
+	}
+
+}
---
+++ background/src/test/java/work/allwens/unionbbs/unionbbs/UnionbbsApplicationTests.java
@@ -0,0 +1,13 @@
+package work.allwens.unionbbs.unionbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnionbbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
