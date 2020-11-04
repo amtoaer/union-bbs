529337be565ac161b62ac0a29a8a09726a1ca709
amtoaer <amtoaer@outlook.com>
2020-11-04 22:05:04 +0800

    feat: 加入登陆注册的请求体bean

---
+++ backend/src/main/java/work/allwens/unionbbs/unionbbs/request/Register.java
@@ -0,0 +1,32 @@
+package work.allwens.unionbbs.unionbbs.request;
+
+public class Register {
+    private String name;
+    private String account;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
