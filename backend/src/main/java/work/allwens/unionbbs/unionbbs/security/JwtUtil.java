85cd87a639c38cf8cf0fe29dcbb007c0671d210f
amtoaer <amtoaer@outlook.com>
2020-11-05 14:43:10 +0800

    feat: 添加JWT认证以及登陆过滤器，保护api

---
+++ backend/src/main/java/work/allwens/unionbbs/unionbbs/security/JwtUtil.java
@@ -0,0 +1,39 @@
+package work.allwens.unionbbs.unionbbs.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import java.util.Date;
+
+public class JwtUtil {
+    // token过期时间为3天
+    private static final long expireTime = 60 * 60 * 24 * 3 * 1000;
+
+    public static boolean verify(String token, long id, String secret) {
+        try {
+            Algorithm algo = Algorithm.HMAC256(secret);
+            var verifier = JWT.require(algo).withClaim("id", id).build();
+            verifier.verify(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static long getId(String token) {
+        try {
+            return JWT.decode(token).getClaim("id").asLong();
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public static String sign(long id, String secret) {
+        try {
+            Date date = new Date(System.currentTimeMillis() + expireTime);
+            var algo = Algorithm.HMAC256(secret);
+            return JWT.create().withClaim("id", id).withExpiresAt(date).sign(algo);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
---
+++ backend/src/main/java/work/allwens/unionbbs/unionbbs/security/LoginFilter.java
@@ -0,0 +1,64 @@
+package work.allwens.unionbbs.unionbbs.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.stereotype.Component;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.annotation.PostConstruct;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import work.allwens.unionbbs.unionbbs.dao.UserDao;
+
+@Component
+public class LoginFilter extends FilterRegistrationBean<Filter> {
+    @Autowired
+    UserDao userDao;
+
+    @PostConstruct
+    public void init() {
+        setFilter(new ApiFilter());
+        setUrlPatterns(List.of("/api/users/*"));
+    }
+
+    class ApiFilter implements Filter {
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                throws IOException, ServletException {
+            HttpServletRequest req = (HttpServletRequest) request;
+            // 拿到token
+            String token = req.getHeader("Authorization");
+            long id;
+            // 正常拿到id
+            if ((id = JwtUtil.getId(token)) != -1) {
+                var tmp = userDao.getById(id);
+                // 通过验证则继续处理
+                if (JwtUtil.verify(token, id, tmp.getUpassword())) {
+                    chain.doFilter(request, response);
+                } else {
+                    HttpServletResponse resp = (HttpServletResponse) response;
+                    resp.setStatus(401);
+                    resp.setContentType("application/json;charset=UTF-8");
+                    ObjectMapper mapper = new ObjectMapper();
+                    resp.getWriter().write(
+                            mapper.writeValueAsString(Map.of("error", "Unauthorized", "message", "token已过期，请重新登录")));
+                }
+            } else {
+                HttpServletResponse resp = (HttpServletResponse) response;
+                resp.setStatus(401);
+                resp.setContentType("application/json;charset=UTF-8");
+                ObjectMapper mapper = new ObjectMapper();
+                resp.getWriter().write(
+                        mapper.writeValueAsString(Map.of("error", "Unauthorized", "message", "请求未包含token，认证失败")));
+            }
+        }
+    }
+}
