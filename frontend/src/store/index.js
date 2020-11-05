d85469d30e003e016928538361ce387fc8394b91
amtoaer <amtoaer@outlook.com>
2020-11-05 23:20:07 +0800

    feat: 在vuex中添加login和register动作

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,38 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { login, register } from "../utils/request";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    user: {},
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user;
+    },
+  },
+  actions: {
+    async login({ commit }, user) {
+      const { data } = await login(user);
+      if (data.error) {
+        return Promise.reject(data);
+      } else {
+        commit("setUser", data.user);
+        return Promise.resolve(data.token);
+      }
+    },
+
+    async register({ commit }, user) {
+      const { data } = await register(user);
+      if (data.error) {
+        return Promise.reject(data);
+      } else {
+        commit("setUser", data.user);
+        return Promise.resolve(data.token);
+      }
+    },
+  },
+  modules: {},
 });
