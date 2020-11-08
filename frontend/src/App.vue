e69c1a4681df302ad24cd2a5b921bb01380fefce
amtoaer <amtoaer@outlook.com>
2020-11-08 16:42:01 +0800

    feat: 添加网站的顶栏和侧栏

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,39 +1,93 @@
 <template>
   <v-app>
-    <input type="text" v-model="account" />
-    <input type="text" v-model="password" />
-    <button @click="login">登录</button>
-    <button @click="ping">获取用户信息</button>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list shaped>
+        <v-list-item-group color="primary" mandatory>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            @click="$router.push({ path: item.path })"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon" :color="item.color"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="blue darken-3"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">{{ title }}</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon large>
+        <v-avatar size="32px" item>
+          <v-img :src="user.uavatar" :alt="user.uname"></v-img
+        ></v-avatar>
+      </v-btn>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon large v-bind="attrs" v-on="on" :disabled="!isLogin">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
-
-  data: () => ({
-    account: "",
-    password: "",
-  }),
-  methods: {
-    login() {
-      this.$store
-        .dispatch("login", { account: this.account, password: this.password })
-        .then((token) => {
-          alert(token);
-          localStorage.setItem("token", token);
-        });
+  computed: {
+    title: function() {
+      return this.$store.state.title;
     },
-    ping() {
-      this.axios
-        .request({
-          url: "/api/users/1",
-          method: "GET",
-        })
-        .then((data) => {
-          alert(data);
-        });
+    user: function() {
+      return this.$store.state.user;
+    },
+    isLogin: function() {
+      return this.$store.state.isLogin;
     },
   },
+  data: () => ({
+    drawer: null,
+    items: [
+      {
+        icon: "mdi-account-box",
+        text: "登录",
+        color: "pink lighten-1",
+        path: "/login",
+      },
+      {
+        icon: "mdi-account-plus",
+        text: "注册",
+        color: "pink lighten-1",
+        path: "/register",
+      },
+    ],
+  }),
 };
 </script>
