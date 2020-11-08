a5501265479e2020cbcef1389cdd65678b316962
amtoaer <amtoaer@outlook.com>
2020-11-08 16:44:04 +0800

    feat: 尝试完成帖子列表渲染

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container fluid>
+    <v-list>
+      <v-list-item v-for="post in posts" :key="post.id">
+        <v-list-item-avatar>
+          <v-img :src="post.avatar"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title v-text="post.username"></v-list-item-title>
+          <v-list-item-subtitle v-text="post.title"></v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-icon>
+          <v-icon>mdi-star</v-icon>
+          {{ post.comment }}
+        </v-list-item-icon>
+      </v-list-item>
+    </v-list>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    posts: [
+      {
+        id: 1,
+        username: "amtoaer",
+        avatar:
+          "http://cdn.jsdelivr.net/gh/amtoaer/blogcdn@master/img/custom/amtoaer.png",
+        title: "这是第一篇文章！",
+        date: "2020.1.27",
+        comment: 10,
+      },
+    ],
+  }),
 };
 </script>
