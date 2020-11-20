8a035c41dc3b7cab0bbea745e328741243e7948a
amtoaer <amtoaer@outlook.com>
2020-11-20 18:59:43 +0800

    feat: 实现帖子和评论的数据访问层

--- backend/src/main/java/work/allwens/unionbbs/unionbbs/dao/CommentDao.java
+++ backend/src/main/java/work/allwens/unionbbs/unionbbs/dao/CommentDao.java
@@ -2,6 +2,26 @@ package work.allwens.unionbbs.unionbbs.dao;
 
 import work.allwens.unionbbs.unionbbs.entity.Comment;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 public class CommentDao extends AbstractDao<Comment> {
 
+    public List<Comment> getByPage(long pid, int index) {
+        // 查找帖子id为pid的第index页评论
+        return getJdbcTemplate().query(
+                "SELECT comments.id,uname,uavatar,ccontent,cdate FROM comments JOIN users WHERE comments.uid = users.id AND comments.pid = ? ORDER BY cdate LIMIT 10 OFFSET ?;",
+                this.rowMapper, pid, 10 * (index - 1));
+    }
+
+    public long getTotalPage(long pid) {
+        long count = getJdbcTemplate().queryForObject("SELECT COUNT(*) FROM comments WHERE comments.pid=?;", Long.class,
+                pid);
+        return (long) Math.ceil((double) count / 10);
+    }
+
+    public void newComment(long uid, long pid, String content) {
+        getJdbcTemplate().update("INSERT INTO comments(uid,pid,ccontent,cdate) VALUES (?,?,?,?);", uid, pid, content,
+                new Timestamp(System.currentTimeMillis()));
+    }
 }
--- backend/src/main/java/work/allwens/unionbbs/unionbbs/dao/PostDao.java
+++ backend/src/main/java/work/allwens/unionbbs/unionbbs/dao/PostDao.java
@@ -1,7 +1,48 @@
 package work.allwens.unionbbs.unionbbs.dao;
 
-import work.allwens.unionbbs.unionbbs.entity.Post;
+import java.util.List;
 
+import work.allwens.unionbbs.unionbbs.entity.Post;
+import java.sql.Timestamp;
+import java.sql.PreparedStatement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
 public class PostDao extends AbstractDao<Post> {
+	@Autowired
+	CommentDao commentDao;
+
+	@Override
+	public List<Post> getByPage(int index) {
+		return getJdbcTemplate().query(
+				"SELECT posts.id,uname,uavatar,ptitle,pdate,pcomment FROM posts JOIN users ON posts.uid=users.id ORDER BY pdate DESC LIMIT 10 OFFSET ?;",
+				this.rowMapper, 10 * (index - 1));
+	}
+
+	public long getTotalPage() {
+		long count = getJdbcTemplate().queryForObject("SELECT COUNT(*) FROM posts;", Long.class);
+		return (long) Math.ceil((double) count / 10);
+	}
+
+	public void newPost(long uid, String title, String content) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		getJdbcTemplate().update(connection -> {
+			PreparedStatement ps = connection
+					.prepareStatement("INSERT INTO posts(uid,ptitle,pdate,pcomment) VALUES(?,?,?,?);");
+			ps.setLong(1, uid);
+			ps.setString(2, title);
+			ps.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+			ps.setInt(4, 0);
+			return ps;
+		}, keyHolder);
+		long pid = keyHolder.getKey().longValue();
+		commentDao.newComment(uid, pid, content);
+	}
 
 }
