45a6ded9e365a47347635a9802ba714a072d8aea
amtoaer <amtoaer@outlook.com>
2020-11-20 20:29:27 +0800

    feat(home): 调整帖子列表的渲染策略，加入axios异步请求

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,37 +1,76 @@
 <template>
   <v-container fluid>
     <v-list>
-      <v-list-item v-for="post in posts" :key="post.id">
+      <v-list-item
+        v-for="post in posts"
+        :key="post.id"
+        @click="$router.push({ path: `/post/${post.id}` })"
+      >
         <v-list-item-avatar>
-          <v-img :src="post.avatar"></v-img>
+          <v-img :src="post.uavatar"></v-img>
         </v-list-item-avatar>
         <v-list-item-content>
-          <v-list-item-title v-text="post.username"></v-list-item-title>
-          <v-list-item-subtitle v-text="post.title"></v-list-item-subtitle>
+          <v-list-item-title v-text="post.uname"></v-list-item-title>
+          <v-list-item-subtitle v-text="post.ptitle"></v-list-item-subtitle>
         </v-list-item-content>
         <v-list-item-icon>
-          <v-icon>mdi-star</v-icon>
-          {{ post.comment }}
+          <v-icon>mdi-comment-processing-outline</v-icon>
+          {{ post.pcomment }}
         </v-list-item-icon>
       </v-list-item>
     </v-list>
+    <div class="text-center">
+      <v-pagination
+        v-model="page"
+        :length="totalPage"
+        prev-icon="mdi-menu-left"
+        next-icon="mdi-menu-right"
+      ></v-pagination>
+    </div>
   </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
-    posts: [
-      {
-        id: 1,
-        username: "amtoaer",
-        avatar:
-          "http://cdn.jsdelivr.net/gh/amtoaer/blogcdn@master/img/custom/amtoaer.png",
-        title: "这是第一篇文章！",
-        date: "2020.1.27",
-        comment: 10,
-      },
-    ],
+    posts: [],
+    page: 1,
+    totalPage: 1,
   }),
+  methods: {
+    // 用于更新当前所处的页
+    updatePage: function() {
+      this.axios
+        .request({
+          url: `/api/posts/page/${this.page}`,
+          method: "GET",
+        })
+        .then((resp) => {
+          this.posts = resp.data;
+        });
+    },
+
+    // 用于得到总页数
+    getTotalPage: function() {
+      this.axios
+        .request({
+          url: "/api/page/posts",
+          method: "GET",
+        })
+        .then((resp) => {
+          this.totalPage = resp.data.count;
+        });
+    },
+  },
+  watch: {
+    page: function() {
+      this.updatePage();
+    },
+  },
+  created: function() {
+    this.$store.commit("setTitle", "主页");
+    this.getTotalPage();
+    this.updatePage();
+  },
 };
 </script>
