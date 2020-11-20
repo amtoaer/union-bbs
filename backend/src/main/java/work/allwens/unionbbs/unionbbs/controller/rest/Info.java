0b217bec2c8a307f63af40552e3a5b4760e13722
amtoaer <amtoaer@outlook.com>
2020-11-20 19:02:11 +0800

    feat(api): 暴露更多的api，基本完成后端设计

--- backend/src/main/java/work/allwens/unionbbs/unionbbs/controller/rest/Info.java
+++ backend/src/main/java/work/allwens/unionbbs/unionbbs/controller/rest/Info.java
@@ -2,6 +2,10 @@ package work.allwens.unionbbs.unionbbs.controller.rest;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,11 +26,18 @@ public class Info {
     @Autowired
     UserDao userDao;
 
-    @GetMapping("/users/{id}")
-    public User getUserById(@PathVariable("id") long id) {
-        return userDao.getById(id);
-    }
+    @Autowired
+    PostDao postDao;
 
+    @Autowired
+    CommentDao commentDao;
+
+    /**
+     * 登陆接口
+     * 
+     * @param body
+     * @return
+     */
     @PostMapping("/login")
     public Map<String, Object> userLogin(@RequestBody Login body) {
         try {
@@ -37,6 +48,12 @@ public class Info {
         }
     }
 
+    /**
+     * 注册接口
+     * 
+     * @param body
+     * @return
+     */
     @PostMapping("/register")
     public Map<String, Object> userRegister(@RequestBody Register body) {
         try {
@@ -45,5 +62,83 @@ public class Info {
         } catch (Exception e) {
             return Map.of("error", "REGISTER_FAILED", "message", e.getMessage());
         }
+    }
+
+    /**
+     * 返回个人信息
+     */
+    @GetMapping("/users/myInfo")
+    public User getMyInfo(HttpServletRequest req) {
+        return (User) req.getAttribute("author");
+    }
+
+    /**
+     * 返回第index页的帖子列表
+     * 
+     * @param index
+     * @return
+     */
+    @GetMapping("/posts/page/{index}")
+    public List<Post> getPostByPage(@PathVariable("index") int index) {
+        return postDao.getByPage(index);
+    }
+
+    /**
+     * 得到帖子列表页数
+     * 
+     * @return
+     */
+    @GetMapping("/page/posts")
+    public Map<String, Object> getPostTotalPage() {
+        return Map.of("count", postDao.getTotalPage());
+    }
+
+    /**
+     * 得到编号为pid的帖子的评论列表页数
+     * 
+     * @param pid
+     * @return
+     */
+    @GetMapping("/page/posts/{pid}")
+    public Map<String, Object> getCommentTotalPage(@PathVariable("pid") long pid) {
+        return Map.of("count", commentDao.getTotalPage(pid));
+    }
+
+    /**
+     * 得到编号为pid的帖子的第index页评论
+     * 
+     * @param pid
+     * @param index
+     * @return
+     */
+    @GetMapping("/posts/{pid}/page/{index}")
+    public List<Comment> getCommentByPage(@PathVariable("pid") long pid, @PathVariable("index") int index) {
+        return commentDao.getByPage(pid, index);
+    }
+
+    /**
+     * 发帖子
+     * 
+     * @param body
+     * @param req
+     */
+    @PostMapping("/posts/new")
+    public void newPost(@RequestBody work.allwens.unionbbs.unionbbs.request.Post body, HttpServletRequest req) {
+        // 获取绑定在请求中的用户信息(详情见过滤器)
+        User author = (User) req.getAttribute("author");
+        // 加入新的帖子
+        postDao.newPost(author.getId(), body.getTitle(), body.getContent());
+    }
+
+    /**
+     * 发评论
+     * 
+     * @param body
+     * @param req
+     */
+    @PostMapping("/comments/new")
+    public void newComment(@RequestBody work.allwens.unionbbs.unionbbs.request.Comment body, HttpServletRequest req) {
+        User author = (User) req.getAttribute("author");
+        commentDao.newComment(author.getId(), body.getPid(), body.getContent());
     }
 }
