efeac4a427f09dba774f2df5a99d92451c390e75
amtoaer <amtoaer@outlook.com>
2020-11-20 19:01:38 +0800

    feat(request): 添加新帖子和新评论的post请求体

---
+++ backend/src/main/java/work/allwens/unionbbs/unionbbs/request/Comment.java
@@ -0,0 +1,23 @@
+package work.allwens.unionbbs.unionbbs.request;
+
+public class Comment {
+    long pid;
+    String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getPid() {
+        return pid;
+    }
+
+    public void setPid(long pid) {
+        this.pid = pid;
+    }
+
+}
---
+++ backend/src/main/java/work/allwens/unionbbs/unionbbs/request/Post.java
@@ -0,0 +1,24 @@
+package work.allwens.unionbbs.unionbbs.request;
+
+public class Post {
+    // 新增帖子的请求参数
+    String title;
+    String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
