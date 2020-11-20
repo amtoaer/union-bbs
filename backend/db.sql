4848dabdf8c75c78dd582667a0bb20372df525b7
amtoaer <amtoaer@outlook.com>
2020-11-20 18:57:13 +0800

    fix(sql): 修改数据库设计，使其更易实现（写 不 完 了）

--- backend/db.sql
+++ backend/db.sql
@@ -1,9 +1,7 @@
 -- 创建reatforum数据库
 DROP DATABASE IF EXISTS unionbbs;
 CREATE DATABASE unionbbs;
-
 USE unionbbs;
-
 -- 创建用户表
 CREATE TABLE users(
     id BIGINT AUTO_INCREMENT NOT NULL,
@@ -14,21 +12,31 @@ CREATE TABLE users(
     usign VARCHAR(100) DEFAULT "" NOT NULL,
     upermission INT DEFAULT 2 NOT NULL,
     PRIMARY KEY (id)
-)CHARSET=UTF8;
-
-INSERT INTO users VALUES (1,"amtoaer","amtoaer","Liuwenxing1!","abc","ceshi",1);
-INSERT INTO users VALUES (2,"test","test","TEST!","sdakljg","sldkgj",2);
-
-
+) CHARSET = UTF8;
+INSERT INTO users
+VALUES (
+        1,
+        'amtoaer',
+        'amtoaer',
+        'Liuwenxing1!',
+        'https://cdn.jsdelivr.net/gh/amtoaer/blogcdn@master/img/custom/amtoaer.png',
+        '测试签名',
+        1
+    );
 -- 创建主题信息表
 CREATE TABLE posts(
     id BIGINT AUTO_INCREMENT NOT NULL,
+    uid BIGINT NOT NULL,
     ptitle VARCHAR(50) NOT NULL,
     pdate DATE NOT NULL,
     pcomment INT NOT NULL,
-    PRIMARY KEY(id)
-)CHARSET=UTF8;
-
+    PRIMARY KEY(id),
+    FOREIGN KEY (uid) REFERENCES users(id)
+) CHARSET = UTF8;
+INSERT INTO posts
+VALUES (1, 1, '这是第一篇文章！', '2020-11-11', 0);
+INSERT INTO posts
+VALUES (2, 1, '这是我的第二篇文章', '2020-11-19', 0);
 -- 创建评论信息表
 CREATE TABLE comments(
     id BIGINT AUTO_INCREMENT NOT NULL,
@@ -36,8 +44,15 @@ CREATE TABLE comments(
     pid BIGINT NOT NULL,
     ccontent VARCHAR(300) NOT NULL,
     cdate DATE NOT NULL,
-    cpre BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (uid) REFERENCES users(id),
     FOREIGN KEY (pid) REFERENCES posts(id)
-)CHARSET=UTF8;
+) CHARSET = UTF8;
+INSERT INTO comments
+VALUES (1, 1, 1, '这是第一篇文章的内容', '2020-11-11');
+INSERT INTO comments
+VALUES (2, 1, 1, '你咋回事啊', '2020-11-12');
+INSERT INTO comments
+VALUES (3, 1, 2, '这是第二篇帖子的内容', '2020-11-19');
+INSERT INTO comment
+VALUES (4, 1, 2, '这是第二篇帖子的评论', '2020-11-20');
