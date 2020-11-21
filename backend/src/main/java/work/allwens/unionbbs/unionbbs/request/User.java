237597f6de586ba7d54dda78fd7bd510d64c19df
amtoaer <amtoaer@outlook.com>
2020-11-22 00:04:28 +0800

    feat(request): 加入用户信息更新的请求体

---
+++ backend/src/main/java/work/allwens/unionbbs/unionbbs/request/User.java
@@ -0,0 +1,41 @@
+package work.allwens.unionbbs.unionbbs.request;
+
+public class User {
+    private String uname;
+    private String uaccount;
+    private String upassword;
+    private String uavatar;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUaccount() {
+        return uaccount;
+    }
+
+    public void setUaccount(String uaccount) {
+        this.uaccount = uaccount;
+    }
+
+    public String getUpassword() {
+        return upassword;
+    }
+
+    public void setUpassword(String upassword) {
+        this.upassword = upassword;
+    }
+
+    public String getUavatar() {
+        return uavatar;
+    }
+
+    public void setUavatar(String uavatar) {
+        this.uavatar = uavatar;
+    }
+
+}
