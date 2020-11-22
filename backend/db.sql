c5b7bf66fe069e4e9178c445adf436a4e67095c3
amtoaer <amtoaer@outlook.com>
2020-11-22 16:42:26 +0800

    fix(sql): 修复sql语句中的错误

--- backend/db.sql
+++ backend/db.sql
@@ -8,7 +8,7 @@ CREATE TABLE users(
     uname VARCHAR(20) NOT NULL,
     uaccount VARCHAR(20) NOT NULL,
     upassword VARCHAR(20) NOT NULL,
-    uavatar VARCHAR(150) DEFAULT "https://gravatar.loli.net/avatar/d41d8cd98f00b204e9800998ecf8427e?s=40&d=mp" NOT NULL,
+    uavatar VARCHAR(150) DEFAULT 'https://gravatar.loli.net/avatar/d41d8cd98f00b204e9800998ecf8427e?s=40&d=mp' NOT NULL,
     PRIMARY KEY (id)
 ) CHARSET = UTF8;
 INSERT INTO users
@@ -17,7 +17,7 @@ VALUES (
         'amtoaer',
         'amtoaer',
         'Liuwenxing1!',
-        'https://cdn.jsdelivr.net/gh/amtoaer/blogcdn@master/img/custom/amtoaer.png',
+        'https://cdn.jsdelivr.net/gh/amtoaer/blogcdn@master/img/custom/amtoaer.png'
     );
 -- 创建主题信息表
 CREATE TABLE posts(
@@ -30,9 +30,9 @@ CREATE TABLE posts(
     FOREIGN KEY (uid) REFERENCES users(id)
 ) CHARSET = UTF8;
 INSERT INTO posts
-VALUES (1, 1, '这是第一篇文章！', '2020-11-11 10:10:10', 0);
+VALUES (1, 1, '这是第一篇文章！', '2020-11-11 10:10:10', 1);
 INSERT INTO posts
-VALUES (2, 1, '这是我的第二篇文章', '2020-11-19 20:20:20', 0);
+VALUES (2, 1, '这是我的第二篇文章', '2020-11-19 20:20:20', 1);
 -- 创建评论信息表
 CREATE TABLE comments(
     id BIGINT AUTO_INCREMENT NOT NULL,
